47b30fa91899a9a3451c48e8be4f28e236f378b6
Alexandros <aldor94@gmail.com>
2016-07-06 22:27:58 +0300

    Change logo on space status change

---
+++ MinisterIn.js
@@ -0,0 +1,41 @@
+var isOpen = false;
+
+function updateIcon() {
+  chrome.browserAction.setIcon({
+  path: isOpen ? {
+    48: "icons/o_48x48.png",
+    96: "icons/o_96x96.png"
+  } : {
+    48: "icons/c_48x48.png",
+    96: "icons/c_96x96.png"
+  }
+});
+}
+
+function checkStatus(text){
+  num = parseInt(text);
+  if (num > 0) {
+    isOpen = true;
+  }else {
+    isOpen = false;
+  }
+  updateIcon();
+}
+
+function httpGetAsync(theUrl, callback)
+{
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  };
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous
+  xmlHttp.send(null);
+}
+
+function timer(){
+  httpGetAsync("https://www.techministry.gr/hackers.txt", checkStatus);
+}
+
+httpGetAsync("https://www.techministry.gr/hackers.txt", checkStatus);
+setInterval(timer, 300000);
